a0ef8948dd2a4e0baf90a6cc67031d5f8fee7286
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-07-20 06:56:35 +0000

    Sat, Jul 20, 2024, 12:26 PM +05:30

--- app.py
+++ app.py
@@ -8,3 +8,10 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = SQL("sqlite:///data.db")
+
+
