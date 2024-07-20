f205df26c5ef766aa993c18dc339a3ca3fd221f5
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-07-20 06:54:49 +0000

    Sat, Jul 20, 2024, 12:24 PM +05:30

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import os
+import time
+
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-
+app = Flask(__name__)
 
