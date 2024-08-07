0f27da2fc00142c95a9398b762d070be0bcd4288
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-08-07 15:28:48 +0000

    Overall update with backend

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import time
 
 from cs50 import SQL
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, flash
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -16,7 +16,7 @@ Session(app)
 
 db = SQL("sqlite:///data.db")
 
-@app.route("/")
+@app.route('/home')
 @login_required
 def index():
     return render_template('index.html')
@@ -26,16 +26,27 @@ def index():
 def login():
     if request.method == 'POST':
         username = request.form['username']
+        password = request.form['password']
 
-        user = db.execute("SELECT * FROM users WHERE username = ?", username)
-        if user:
-            session['user_id'] = user[0]['username']
-            return redirect("/")
+        if not username:
+            flash('Username required!')
+
+        elif not password:
+            flash('Password required!')
+
         else:
-            return "Invalid username", 400
+            user = db.execute("SELECT * FROM users WHERE username = ?", username)
+
+            if len(user) != 1 or not check_password_hash(user[0]['password'],password):
+                flash('Invalid Username or Password')
+                print('Invalid Username or Password')
+            else:
+                session['user_id'] = user[0]['id']
+                return redirect('/home')
+
+
     else:
         return render_template('login.html')
-
 
 
 @app.route('/logout', methods=['POST'])
@@ -43,3 +54,31 @@ def logout():
     session.clear()
     return redirect('/login')
 
+@app.route('/signup', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password1 = request.form['password1']
+        password2 = request.form['password2']
+
+        if not username:
+            flash('Username required!')
+
+        elif not password1:
+            flash('Password required!')
+
+        elif not password2:
+            flash('Confirm Password required!')
+
+        elif password1 != password2:
+            flash('Passwords do not match!')
+
+        else:
+            hash = generate_password_hash(password1)
+
+            db.execute("INSERT INTO users (username, password) VALUES(?, ?)", username, hash)
+            flash('Registeration Succesful!')
+            return redirect('/login')
+
+    else:
+        return render_template('signup.html')
